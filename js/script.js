d3e9061855e961778b88c2dd842453b9b750122f
nikotheflow <bboyfoxxx@yandex.ru>
2022-09-02 19:20:09 +0300

    Made header adaptive and animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let heroBehind = document.querySelector('.hero__image-behind');
-  let heroTextBlock = document.querySelector('.hero__text-block');
+  const body = document.querySelector('body'),
+    heroBehind = document.querySelector('.hero__image-behind'),
+    headerNavbar = document.querySelector('.header__navbar'),
+    burgerBtn = document.querySelector('.burger-btn'),
+    burgerBtnText = document.querySelector('.burger-btn__text'),
+    navbarMenu = document.querySelector('.navbar__menu');
+
+  let scrollWidth = window.innerWidth - body.clientWidth,
+    burgerBtnPaddingRight = parseInt(window.getComputedStyle(burgerBtn).paddingRight);
 
   window.addEventListener('scroll', function () {
     let scrollValue = window.scrollY;
-
-    // heroTextBlock.style.transform = `translateY(${scrollValue}px)`;
     heroBehind.style.transform = `translateY(-${scrollValue / 5}px)`;
   });
+
+  window.addEventListener('resize', () => {
+    burgerBtn.classList.remove('active');
+    navbarMenu.classList.remove('active');
+    body.classList.remove('lock');
+
+    removeScrollPadding();
+  });
+
+  setTimeout(() => {
+    headerNavbar.style.top = 0;
+  }, 500);
+
+  burgerBtn.addEventListener('click', function () {
+    burgerBtn.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+    body.classList.toggle('lock');
+
+    burgerBtnText.innerText = burgerBtn.classList.contains('active') ? 'закрыть' : 'меню';
+
+    if (body.classList.contains('lock')) {
+      body.style.paddingRight = `${scrollWidth}px`;
+      burgerBtn.style.paddingRight = `${scrollWidth + burgerBtnPaddingRight}px`;
+    } else {
+      removeScrollPadding();
+    }
+  });
+
+  function removeScrollPadding() {
+    body.style.paddingRight = 0;
+    burgerBtn.style.paddingRight = `${burgerBtnPaddingRight}px`;
+  }
 });
